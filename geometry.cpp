657a66e3a977607b421085e03cbc6093b73b5d96
Smirnie <33089050+Smirnie@users.noreply.github.com>
2017-11-22 07:35:41 -0800

    korrektur

---
+++ geometry.cpp
@@ -0,0 +1,237 @@
+#include "geometry.hpp"
+
+/// Constructs a default geometry:
+// driven cavity with 128 x 128 grid, no-slip boundary conditions
+// as shown below
+//
+//      u=1, v=0
+//    -------------
+//    |           |
+// u=0|           |u=0
+// v=0|           |v=0
+//    |           |
+//    |           |
+//    -------------
+//      u=0, v=0
+Geometry::Geometry() {
+    Load("default.geom");
+	// Load("actual.geom");
+
+	_h = multi_real_t(_length[0] / (_size[0] - 2), _length[1] / (_size[1] - 2));
+}
+
+Geometry::Geometry(const Communicator *comm) {
+    Load("default.geom");
+    _comm = comm;
+    _h = multi_real_t(_blength[0] / (_bsize[0] - 2), _blength[1] / (_bsize[1] - 2));
+    // TODO: insert statements here
+}
+
+/// Loads a geometry from a file
+void Geometry::Load(const char * file) {
+    
+    FILE* handle = fopen(file,"r");
+    char name[20];
+    multi_real_t inval_real;
+    multi_index_t inval_index;
+    while (!feof(handle)) {
+        
+        if (!fscanf(handle, "%s =", name)) continue;
+        
+        if (strcmp(name,"bsize") == 0) {
+            if (fscanf(handle," %i %i\n",&inval_index[0],&inval_index[1])) {
+                _bsize[0] = inval_index[0]+2;
+                _bsize[1] = inval_index[1]+2;
+            }
+            continue;
+        }
+        
+        if (strcmp(name,"blength") == 0) {
+            if (fscanf(handle," %lf %lf\n",&inval_real[0],&inval_real[1])) {
+                _blength[0] = inval_real[0];
+                _blength[1] = inval_real[1];
+            }
+            continue;
+        }
+        if (strcmp(name,"velocity") == 0) {
+            if (fscanf(handle," %lf %lf\n",&inval_real[0],&inval_real[1])) {
+                _velocity[0] = inval_real[0];
+                _velocity[1] = inval_real[1];
+            }
+            continue;
+        }
+        if (strcmp(name,"pressure") == 0) {
+            if (fscanf(handle," %lf\n",&inval_real[0]))
+                _pressure = inval_real[0];
+            continue;
+        }
+        //funktioniert nur wenn das break drin ist sonst hängt er sich an der |#- geometry definition in default.geom auf.
+        // |#- geometry definition in default.geom wird nicht eingelesen.
+        if (strcmp(name,"geometry") == 0) {
+            break;
+        }
+        
+    }
+	fclose(handle);
+}
+
+
+/// Returns the number of cells in each dimension
+const multi_index_t & Geometry::Size() const {
+	return _size;
+}
+
+/// Returns the total number of cells in each dimension
+const multi_index_t & Geometry::TotalSize() const {
+        return _bsize;
+}
+
+/// Returns the length of the domain
+const multi_real_t & Geometry::Length() const {
+	return _length;
+}
+
+/// Returns the total length of the domain
+const multi_real_t & Geometry::TotalLength() const {
+        return _blength;
+}    
+
+/// Returns the meshwidth
+const multi_real_t & Geometry::Mesh() const {
+	return _h;
+}
+
+/// Returns the initial velocity
+const multi_real_t & Geometry::Velocity() const {
+	return _velocity;
+}
+
+/// Returns the initial pressure
+const real_t & Geometry::Pressure() const {
+	return _pressure;
+}
+
+
+/// Updates the velocity field u
+void Geometry::Update_U(Grid * u) const {
+	// see script, p. 17
+	BoundaryIterator bit = BoundaryIterator(this);
+    
+    // Iteration over right boundary
+    bit.SetBoundary(1);
+    if(_comm->isRight()){
+	for(bit.First(); bit.Valid(); bit.Next()) {
+		u->Cell(bit.Left()) = 0.0;
+	}
+    }
+    
+    // Iteration over left boundary
+    bit.SetBoundary(3);
+    if(_comm->isLeft()){
+        bit.First();
+        u->Cell(bit.Down()) = 0.0; // Lower left corner
+        for(bit.First(); bit.Valid(); bit.Next()) {
+		u->Cell(bit) = 0.0;
+	}
+    }
+    
+    // Iteration over upper boundary
+    bit.SetBoundary(0);
+    if(_comm->isTop()){
+        bit.First();
+	u->Cell(bit.Left()) = 2.0; // Upper left corner
+	for(bit.First(); bit.Valid(); bit.Next()) {
+		u->Cell(bit) = 2.0 - u->Cell(bit.Down()); //2.0
+	}
+    }
+	
+    // Iteration over lower boundary
+    if(_comm->isBottom()){
+	bit.SetBoundary(2);
+	for(bit.First(); bit.Valid(); bit.Next()) {
+		u->Cell(bit) =  - u->Cell(bit.Top());
+	}
+    }
+	
+}
+
+/// Updates the velocity field v
+void Geometry::Update_V(Grid * v) const {
+    // see script, p. 17	
+    BoundaryIterator bit = BoundaryIterator(this);
+    
+    // Iteration over upper boundary
+    bit.SetBoundary(0);
+    if(_comm->isTop()){
+	for(bit.First(); bit.Valid(); bit.Next()) {
+		v->Cell(bit.Down()) = 0.0;
+	}
+    }
+    
+    // Iteration over lower boundary
+    bit.SetBoundary(2);
+    if(_comm->isBottom()){
+        bit.First();
+        v->Cell(bit.Right()) = 0.0; // Lower right corner
+        for(bit.First(); bit.Valid(); bit.Next()) {
+		v->Cell(bit) = 0.0;
+	}
+    }
+    
+    // Iteration over left boundary
+    bit.SetBoundary(3);
+    if(_comm->isLeft()){
+        bit.First();
+        v->Cell(bit.Down()) = 0.0; // Lower left corner
+	for(bit.First(); bit.Valid(); bit.Next()) {
+		v->Cell(bit) = - v->Cell(bit.Right());
+	}
+    }
+    
+    // Iteration over right boundary
+    bit.SetBoundary(1);
+    if(_comm->isRight()){
+	for(bit.First(); bit.Valid(); bit.Next()) {
+		v->Cell(bit) = - v->Cell(bit.Left());
+	}
+    }
+	
+}
+
+/// Updates the pressure field p
+void Geometry::Update_P(Grid * p) const {
+    // see script, p. 20 (p_{0,j} = p{1,j}), ...
+    BoundaryIterator bit = BoundaryIterator(this);
+    
+    // Iteration over upper boundary
+    bit.SetBoundary(0);
+    if(_comm->isTop()){
+        for(bit.First(); bit.Valid(); bit.Next()) {
+		p->Cell(bit) = p->Cell(bit.Down());
+        }
+    }
+    
+    // Iteration over right boundary
+    bit.SetBoundary(1);
+    if(_comm->isRight()){
+	for(bit.First(); bit.Valid(); bit.Next()) {
+		p->Cell(bit) = p->Cell(bit.Left());
+        }
+    }
+    
+    // Iteration over lower boundary
+    bit.SetBoundary(2);
+    if(_comm->isBottom()){
+	for(bit.First(); bit.Valid(); bit.Next()) {
+		p->Cell(bit) = p->Cell(bit.Top());
+        }
+    }
+    
+    // Iteration over left boundary
+    bit.SetBoundary(3);
+    if(_comm->isLeft()){
+	for(bit.First(); bit.Valid(); bit.Next()) {
+		p->Cell(bit) = p->Cell(bit.Right());
+	}
+    }
+}
---
+++ grid.hpp
@@ -0,0 +1,121 @@
+/*
+ * Copyright (C) 2015   Malte Brunn
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+//------------------------------------------------------------------------------
+#ifndef __GRID_HPP
+#define __GRID_HPP
+
+//------------------------------------------------------------------------------
+#ifndef _USE_MATH_DEFINES
+#define _USE_MATH_DEFINES
+#include <cmath>
+#endif // _USE_MATH_DEFINES
+
+#include <iostream>
+
+#include "typedef.hpp"
+#include "geometry.hpp"
+#include "iterator.hpp"
+
+//------------------------------------------------------------------------------
+class Grid {
+public:
+  /// Constructs a grid based on a geometry
+  Grid(const Geometry *geom);
+
+  /// Constructs a grid based on a geometry with an offset
+  // @param geom   Geometry information
+  // @param offset distance of staggered grid point to cell's anchor point;
+  //               (anchor point = lower left corner)
+  Grid(const Geometry *geom, const multi_real_t &offset);
+
+  /// Deletes the grid
+  ~Grid();
+
+  /// prints the grid
+  void print() const;
+
+  ///     Initializes the grid with a value
+  void Initialize(const real_t &value);
+
+  /// Write access to the grid cell at position [it]
+  real_t &Cell(const Iterator &it);
+  /// Read access to the grid cell at position [it]
+  const real_t &Cell(const Iterator &it) const;
+
+  /// Interpolate the value at a arbitrary position
+  real_t Interpolate(const multi_real_t &pos) const;
+
+  /// Computes the left-sided difference quatient in x-dim at [it]
+  real_t dx_l(const Iterator &it) const;
+  /// Computes the right-sided difference quatient in x-dim at [it]
+  real_t dx_r(const Iterator &it) const;
+  /// Computes the left-sided difference quatient in y-dim at [it]
+  real_t dy_l(const Iterator &it) const;
+  /// Computes the right-sided difference quatient in x-dim at [it]
+  real_t dy_r(const Iterator &it) const;
+  /// Computes the central difference quatient of 2nd order in x-dim at [it]
+  real_t dxx(const Iterator &it) const;
+  /// Computes the central difference quatient of 2nd order in y-dim at [it]
+  real_t dyy(const Iterator &it) const;
+  /// Computes the central difference quotient of 1st order in x dim at [it]
+  real_t dx_central(const Iterator & it) const;
+  /// Computes the central difference quotient of 1st order in y dim at [it]
+  real_t dy_central(const Iterator & it) const;
+
+  /// Computes u*du/dx with the donor cell method
+  real_t DC_udu_x(const Iterator &it, const real_t &alpha) const;
+  /// Computes v*du/dy with the donor cell method
+  real_t DC_vdu_y(const Iterator &it, const real_t &alpha, const Grid *v) const;
+  /// Computes u*dv/dx with the donor cell method
+  real_t DC_udv_x(const Iterator &it, const real_t &alpha, const Grid *u) const;
+  /// Computes v*dv/dy with the donor cell method
+  real_t DC_vdv_y(const Iterator &it, const real_t &alpha) const;
+  
+  real_t DC_du2_x(const Iterator & it, const real_t & alpha) const;
+  
+  real_t DC_dv2_y(const Iterator & it, const real_t & alpha) const;
+
+  real_t DC_duv_x(const Iterator & it, const real_t & alpha, const Grid *v) const;
+
+  real_t DC_duv_y(const Iterator & it, const real_t & alpha, const Grid *u) const;
+
+
+  /// Returns the maximal value of the grid
+  real_t Max() const;
+  /// Returns the minimal value of the grid
+  real_t Min() const;
+  /// Returns the absolute maximal value
+  real_t AbsMax() const;
+
+  /// Returns a pointer to the raw data
+  real_t *Data();
+
+  /** Get the offset value of the grid
+  */
+  const multi_real_t & getOffset() const;
+
+  /// Return a pointer to the Geometry
+  const Geometry * getGeometry() const;
+
+private:
+  real_t *_data;
+  multi_real_t _offset;
+  const Geometry *_geom;
+};
+//------------------------------------------------------------------------------
+#endif // __GRID_HPP
